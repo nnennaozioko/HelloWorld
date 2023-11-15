9a2117f7002102ab9f56edf55f2cc3fbe06ed83f
Nelly <nelytino@yahoo.com>
2023-11-15 01:47:25 +0000

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,36 @@
+﻿//https://aka.ms/new-console-template for more information
+
+using System;
+namespace HelloWorld
+{ 
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //int a = 5;
+            //int a = 2, b = 3;
+            //Console.WriteLine(a+b);
+            //float var = 2.5f;
+            //Console.WriteLine(var);
+
+            //String name = "Nelly";
+            //Console.WriteLine(name);
+            //String name1 = "N";
+            //Console.WriteLine(name1);
+            //Console.WriteLine("name is priting:"+ name);
+            int a = 5;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(a);
+
+            }
+        }
+
+
+    }
+
+
+
+
+}
---
+++ HelloWorld/forloop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class forloop
+    {
+        public static void main(String[] args)
+        {
+            int a = 5;
+           // int b = 1000;  
+           // for (int i = 0; i < a; i++)
+            for(int i = 1; i<= 5; i++)
+                Console.WriteLine(a);
+
+
+        }
+    }
+}
